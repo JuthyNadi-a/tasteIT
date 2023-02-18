b6cc2b3b3f1aef8622a819c9582ca5007f4dde5c
Nadia Jahan Juthy <juthynadia@gmail.com>
2023-02-18 09:40:16 +0200

    fixed embold issues

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,4 @@
-import React/* , {useState, useEffect} */ from 'react';
-/* import axios from 'axios'; */
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Button from './Button';
 import classes from './RecipeCard.module.css'
